820388e027ad0b73347ae0b28cbbb84c93cecceb
samihttps <148577681+samihttps@users.noreply.github.com>
2023-12-01 04:52:04 -0800

    sami backend-project

--- main.go
+++ main.go
@@ -6,13 +6,16 @@ func main() {
 conferenceName := "Go Conference"
 const conferenceTickets int= 50
 var remainingTickets uint= 50
-
+bookings := []string{}
 
 fmt.Printf("Welcome to %v booking application\n", conferenceName)
 fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 fmt.Println("Get your tickets here to attend")
 
 
+
+
+ 
 var firstName string
 var lastName string
 var email string
@@ -31,6 +34,12 @@ fmt.Println("Enter your number of tickets: ")
 fmt.Scan(&userTickets)
 
 remainingTickets = remainingTickets - userTickets
+bookings = append(bookings, firstName+ " " + lastName)
+
+fmt.Printf("The whole slice: %v\n", bookings)
+fmt.Printf("The first value: %v\n", bookings[0])
+fmt.Printf("Slice type: %T\n", bookings)
+fmt.Printf("Slice length: %v\n", len(bookings))
 
 fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n",firstName, lastName,userTickets,email) 
 fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
