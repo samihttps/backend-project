0e175e99e9a6e8c28dbd9c145ae50ce04dcb7c1f
samihttps <148577681+samihttps@users.noreply.github.com>
2024-01-22 16:33:14 +0300

    samish back-end & front-end project
    this projects is uses React js , Node.js and socket.io to do chat app.

---
+++ userController.js
@@ -0,0 +1,95 @@
+const userModel = require("../Models/userModel");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
+const jwt = require("jsonwebtoken")
+
+const createToken = ( _id) =>{
+    const jwtkey = process.env.JWT_SECRET_KEY;
+
+    return jwt.sign({ _id }, jwtkey, { expiresIn: "3d" });
+};
+
+const registerUser = async(req, res) => {
+    
+    try{
+
+    const {name, email, password} = req.body;
+
+    let user = await userModel.findOne({ email });
+    
+    if(user) 
+      return res.status(400).json("user with the the given email already exist...");
+    
+    if(!name || !email || !password) 
+      return res.status(400).json("All fields are required...");
+
+     if(!validator.isEmail(email)) 
+      return res.status(400).json("Email must be a valid email...");
+
+    if(!validator.isStrongPassword(password))
+       return res.status(400).json("Password must be a strong password...");
+    
+    user = new userModel({name, email, password});
+    
+    const salt = await bcrypt.gensalt(10)
+    user.password = await bcrypt.hash(user.password, salt);
+
+    await user.save();
+
+    const token = createToken(user._id);
+
+    res.status(200).json({_id:user._id, name: user.name, email,token});
+  } catch(error) 
+{
+    console.log(error);
+    res.status(500).json(error);
+}
+};
+
+const loginUser = async(req, res) =>{
+  const { email, password} = req.body;
+  try {
+    let user = await userModel.findOne({ email });
+
+    if(!user) return res.status(400).json("Invalid email or password...");
+    
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword)
+      return res.status(400).json("Invalid email password...");
+
+      const Token = createToken(user._id);
+
+      res.status(200).json({ id: user.id, name: username, email, token});
+} catch (error) {
+  console.log(error);
+  res.status(500).json(error);
+}
+};
+
+
+const findUser = async(req, res) =>{ 
+  const userId = req.params.userId;
+try{
+ const userId = await userModel.findById(userId);
+
+ res.status(200).json(user);
+}catch(error){
+  console.log(error);
+  res.status(500).json(error);
+}
+};
+
+const getUser = async(req, res) =>{ 
+
+try{
+ const user = await userModel.find();
+
+ res.status(200).json(user);
+}catch(error){
+  console.log(error);
+  res.status(500).json(error);
+}
+};
+module.exports = {registerUser, loginUser, findUser, getUser};
+
